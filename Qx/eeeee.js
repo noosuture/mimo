af559836fe1392a8e3f303f6b78770051624ef19
Nnn <43199306+noosuture@users.noreply.github.com>
2025-12-25 12:00:39 +0800

    Update eeeee.js

--- Qx/eeeee.js
+++ Qx/eeeee.js
@@ -56,7 +56,7 @@ const bundle = {
 };
 
 const list = {
-  'Offsuit': { name: 'premium', id: 'com.offsuit.offline_sub_year_3599', cm: 'sjb' },  //Offsuit德州
+  'Offsuit': { name: 'premium', id: 'com.offsuit.offline_sub_year_3599', cm: 'sja' },  //Offsuit德州
   'BeetleADB': { name: 'beetle_pro', id: 'beetle_lifetime_pro', cm: 'sjc' },  //甲壳虫ADB
   'adbTools': { name: 'pro_lifetime', id: 'com.jy.adbTools.pro_1', cm: 'sjb' },  //adbTools
   'Habitor': { name: 'premium', id: 'habitor_lifetime', cm: 'sjb' },  //Habitor
